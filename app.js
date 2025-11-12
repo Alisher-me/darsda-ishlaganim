ebaf8f67b2bb2f5bc46bece1a64819be6e27e3cb
Alisher <alisher201127@gmail.com>
2025-11-12 17:54:49 +0500

    ina ishladim

---
+++ app.js
@@ -0,0 +1,115 @@
+//1 
+// let f=["apple",'banana','range'];
+// f.push('peach');
+// console.log(f);
+//2
+// let nums=[4,5,6,7];
+// nums.pop()
+// console.log(nums);
+//3
+// let arr=['middle','end'];
+// arr.unshift('start');
+// console.log(arr);
+//4
+// let cars=['bmw','mercedes','tesla'];
+// cars.shift();
+// console.log(cars);
+//5
+// let a=[1,2];
+// let b=[3,4,5];
+// let qoshilgan=a.concat(b);
+// console.log(qoshilgan);
+//6
+// let fruits=['apple','banana','orange','kiwi'];
+// let sliced=fruits.slice(1,2);
+// console.log(sliced);
+//7
+// let tech=['html','css','js','react'];
+// tech.splice(2,1);
+// console.log(tech);
+//8
+// let langs=['html','css','java script'];
+// console.log(langs.indexOf("css"));
+//9
+// let fruits=['apple','banana','apple','orange'];
+// console.log(fruits.lastIndexOf("apple"));
+//10
+// let fruits=['kiwi','banana','orange'];
+// console.log(fruits.includes('kiwi'));
+//11
+// let nums=[3,8,12,5,20];
+// console.log(nums.find(n=>n>10));
+//12
+// let ages=[12,15,22,16];
+// console.log(ages.some(a=>a>18));
+//13
+// let nums=[3,6,8,-2];
+// console.log(nums.every(a=>a>0));
+//14
+// let nums=[1,2,3];
+// console.log(nums.map(a=>a*3));
+//15
+// let words=['cat','elephant','dog','mouse'];
+// console.log(words.filter(word=>word.length>4));
+//16
+// let n=[5,10,15];
+// console.log(n.reduce((a,n)=>a+n));
+//17
+// let names=['zarina','ali','bekzod'];
+// console.log(names.sort());
+// //18
+// let nums=[40,5,25,100];
+// console.log(nums.sort((a,b)=>a-b));
+//19
+// let arr=[1,2,3,4];
+// console.log(arr.reverse());
+//20
+// let nums=[-3,4,-1,6,0];
+// let plus=nums.filter(n=>n>0);
+// let map=plus.map(m=>m*2);
+// let reverse= map.reverse();
+// console.log(reverse);
+//21
+// let fruits=['apple','banana','orange','kiwi'];
+// fruits.splice(1,1)
+// console.log(fruits);
+//22
+// let nums=[2,4,6,8];
+// let kop=nums.map(n=>n*3);
+// let yig=kop.reduce((a,b)=>a+b,0);
+// console.log(yig);
+//23
+// let names=['zarina','ali','bekzod','dilshod'];
+// let sorted=names.sort();
+// console.log(sorted.reverse());
+//24
+// let numbers=[-3,5,-1,0,8,7];
+// console.log(numbers.filter(n=>n>0));
+//25
+// let nums=[4,9,11,15,7];
+// console.log(nums.find(n=>n>10));
+//26
+// let tech=['html','css','js','react'];
+// console.log(tech.includes('react'));
+//27
+// let words=['js','javascript','html','css'];
+// let word2=words.sort((a,b)=>a.length-b.length)
+// console.log(word2.reverse());
+//28
+// let nums=[5,7,10,0,-3];
+// console.log(nums.every(n=>n>0));
+//29
+// let girls=['laylo','zarina'];
+// let boys=['bekzod','ali'];
+// console.log(girls.concat(boys));
+//30
+// let arr=[10,20,30,40,50];
+// console.log(arr.slice(1,3));
+//31
+let fruits=['apple','banana','apple','orange','apple'];
+console.log(fruits.lastIndexOf('apple'));
+
+
+
+
+
